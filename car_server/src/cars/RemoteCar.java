d240f221d157d017b32a9e43c8a881ab7052d28e
Ole Ildsgaard Hougaard (OIH) | VIA <OIH@VIAUC.DK>
2018-09-17 12:31:16 +0100

    Fixed deliberate errors

--- car_server/src/cars/RemoteCar.java
+++ car_server/src/cars/RemoteCar.java
@@ -1,12 +1,16 @@
 package cars;
 
-public class RemoteCar implements Car {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RemoteCar extends UnicastRemoteObject implements Car {
 	private static final long serialVersionUID = 1L;
 	private String model;
 	private int year;
 	private Money price;
 
-	public RemoteCar(String model, int year, Money price) {
+	public RemoteCar(String model, int year, Money price) throws RemoteException {
+		super();
 		this.model = model;
 		this.year = year;
 		this.price = price;
