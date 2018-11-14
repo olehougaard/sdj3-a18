0844745888ea4fa8893847383db6727acd616e93
Ole Ildsgaard Hougaard <olehougaard@gmail.com>
2018-11-14 09:52:30 +0100

    Fixed typing issues in generics

--- rs-client/src/RSTest.java
+++ rs-client/src/RSTest.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.client.ClientConfig;
@@ -17,7 +19,10 @@ public class RSTest {
 		WebTarget target = client.target("http://localhost:8080/rs-json");
 		User user = target.path("users").path("0").request().accept(MediaType.APPLICATION_JSON).get(User.class);
 		System.out.println(user);
-		ArrayList<User> users = target.path("users").request().accept(MediaType.APPLICATION_JSON).get(ArrayList.class);
+		ArrayList<User> users = target.path("users").request().accept(MediaType.APPLICATION_JSON).get(new GenericType<ArrayList<User>>() {});
 		System.out.println(users);
+		User userData = new User(-1, "user@users.com", "adlkfj");
+		User createdUser = target.path("users").request(MediaType.APPLICATION_JSON).post(Entity.json(userData)).readEntity(User.class);
+		System.out.println(createdUser);
 	}
 }
